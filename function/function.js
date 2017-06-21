34d82bb5962720bf33ad84844f7ba6bfb1975989
Pavlo Shchur <pavloshchur@gmail.com>
2017-06-21 23:39:53 +0300

    JavaScript_01

---
+++ function/function.js
@@ -0,0 +1,7 @@
+/**
+ * Created by User on 21.06.2017.
+ */
+
+function sayHello() {
+    document.write("Hello there")
+}
---
+++ function/function_with_parameters.js
@@ -0,0 +1,7 @@
+/**
+ * Created by User on 21.06.2017.
+ */
+
+function sayHello(name, age) {
+    document.write(name + " is " + age + " years old.")
+}
